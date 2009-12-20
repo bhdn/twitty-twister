5bb19f1710f539d46802a185e0307594a80748c9
Bogdano Arendartchuk <debogdano@gmail.com>
2009-12-19 23:12:57 -0200

    Twitter: added list_members method and example
    Signed-off-by: Bogdano Arendartchuk <debogdano@gmail.com>

--- twittytwister/twitter.py
+++ twittytwister/twitter.py
@@ -429,6 +429,9 @@ class Twitter(object):
     def followers_ids(self, delegate, user, params={}, extra_args=None, page_delegate=None):
         return self.__get_maybe_paging('/followers/ids/%s.xml' % (user), delegate, params, txml.PagedIDList, extra_args, page_delegate)
 
+    def list_members(self, delegate, user, list_name, params={}, extra_args=None):
+        return self.__get('/%s/%s/members.xml' % (user, list_name), delegate,
+                params, txml.Users, extra_args)
 
     def show_user(self, user):
         """Get the info for a specific user.
